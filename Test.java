eaba012f6efd97a533681f7ceea1860d962f422d
chandra kiran <85118800+chandrakiran428@users.noreply.github.com>
2023-12-11 17:24:53 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,7 +1,17 @@
 public class Test {
     // main function
-     public static void main(String []args)
-    {
-        System.out.println("Java Program.");
+     public static void main(String[] args) {
+
+    int n = 10, firstTerm = 0, secondTerm = 1;
+    System.out.println("Fibonacci Series till " + n + " terms:");
+
+    for (int i = 1; i <= n; ++i) {
+      System.out.print(firstTerm + ", ");
+
+      // compute the next term
+      int nextTerm = firstTerm + secondTerm;
+      firstTerm = secondTerm;
+      secondTerm = nextTerm;
     }
+  }
 }
