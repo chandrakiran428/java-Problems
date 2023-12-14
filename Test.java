a47d7c617c2cceb158528214f41a16f00b7d405f
chandra kiran <85118800+chandrakiran428@users.noreply.github.com>
2023-12-14 12:48:03 +0530

    Update Test.java
    comitted

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 class Test {
 
   public static void main(String[] args) {
-    String line = "This website is aw3som3.";
+    String line = "MY NAME IS CHAndru  ,123.";
     int vowels = 0, consonants = 0, digits = 0, spaces = 0;
 
     line = line.toLowerCase();
