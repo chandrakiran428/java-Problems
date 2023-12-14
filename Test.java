71514b987a4c28fac688f4f77486f1fbdab071d3
chandra kiran <85118800+chandrakiran428@users.noreply.github.com>
2023-12-14 12:37:35 +0530

    Update Test.java
    program changed

--- Test.java
+++ Test.java
@@ -1,17 +1,37 @@
-public class Test {
-    // main function
-     public static void main(String[] args) {
+class Test {
 
-    int n = 10, firstTerm = 0, secondTerm = 1;
-    System.out.println("Fibonacci Series till " + n + " terms:");
+  public static void main(String[] args) {
+    String line = "This website is aw3som3.";
+    int vowels = 0, consonants = 0, digits = 0, spaces = 0;
 
-    for (int i = 1; i <= n; ++i) {
-      System.out.print(firstTerm + ", ");
+    line = line.toLowerCase();
+    for (int i = 0; i < line.length(); ++i) {
+      char ch = line.charAt(i);
 
-      // compute the next term
-      int nextTerm = firstTerm + secondTerm;
-      firstTerm = secondTerm;
-      secondTerm = nextTerm;
+      // check if character is any of a, e, i, o, u
+      if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+        ++vowels;
+      }
+
+      // check if character is in between a to z
+      else if ((ch >= 'a' && ch <= 'z')) {
+        ++consonants;
+      }
+      
+      // check if character is in between 0 to 9
+      else if (ch >= '0' && ch <= '9') {
+        ++digits;
+      }
+      
+      // check if character is a white space
+      else if (ch == ' ') {
+        ++spaces;
+      }
     }
+
+    System.out.println("Vowels: " + vowels);
+    System.out.println("Consonants: " + consonants);
+    System.out.println("Digits: " + digits);
+    System.out.println("White spaces: " + spaces);
   }
 }
